fcb07926f2ad6057817edd3157105e2f7ea09ef1
baulu <99120677+baulu@users.noreply.github.com>
2024-04-23 15:51:34 +0200

    second

--- funtions/func_financial_projection.R
+++ funtions/func_financial_projection.R
@@ -7,12 +7,13 @@ project_financials <- function(own_capital, total_debt, interest_rate, amortizat
   equity[1] <- own_capital
   debt[1] <- total_debt
   amortization_payments[1] <- total_debt*amortization_rate
+  mortgage_payments[1] <- total_debt* interest_rate
   
   for (i in 2:years) {
-    mortgage_payments[i] <- debt[i - 1] * interest_rate
-    amortization_payments[i] <- debt[i - 1] * amortization_rate
-    equity[i] <- equity[i - 1]
-    debt[i] <- debt[i - 1] - mortgage_payments[i] - amortization_payments[i]
+    mortgage_payments[i] <- debt[1] * interest_rate
+    equity[i] <- equity[1]
+    debt[i] <- debt[i - 1] - amortization_payments[i-1]
+    amortization_payments[i] <- debt[i] * amortization_rate
   }
   
   result <- data.frame(
