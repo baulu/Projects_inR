2a4c9cecc894d6868a9b538edde42d90119f3e17
baulu <99120677+baulu@users.noreply.github.com>
2024-04-23 11:52:46 +0200

    initial

--- scripts/CreateNewProj.R
+++ scripts/CreateNewProj.R
@@ -3,6 +3,7 @@
 # Creating a new Project(GITHub-First approach) and connecting to GIT
 library(usethis)
 library(gitcreds)
+usethis::use_git()
 usethis::create_github_token()
-
+usethis::use_github()
 gitcreds::gitcreds_set() 
